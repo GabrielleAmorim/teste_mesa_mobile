7ef0bc2b17eee4f8e608dbb5337b0b553d633cc7
Gabrielle Amorim <gabrielleporto19@gmail.com>
2020-09-24 21:25:46 -0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/teste_mesa_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.teste_mesa_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/tela_inicial.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TelaInicial extends StatelessWidget{
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: Text('teste')),
+      body: Container(
+        child: Text("Testeeee"),
+      ),
+    );
+  }
+
+
+}
